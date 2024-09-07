072169c0d5d9addd354f1518c1ee9fffd7210dc8
anushka <anushka20gupta03@gmail.com>
2024-09-07 19:14:59 +0530

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,9 @@ import bcryptjs from 'bcryptjs';
 
 export const  signup =async (req, res, next) => {
    const {username,email,password}= req.body; 
-   const hashedPassword = bcryptjs.hashSync(password,10);
+//    const hashedPassword = bcryptjs.hashSync(password,10);
+     const salt = bcryptjs.genSaltSync(10); // Generate a salt value with a cost factor of 10
+     const hashedPassword = bcryptjs.hashSync(password, salt); // Hash the password using the generated salt
    const newUser = new User({username,email,password:hashedPassword});
    try{
         await newUser.save();
