436b6314b82b613b4c1e9f55a27c29349002095e
anushka <anushka20gupta03@gmail.com>
2024-09-27 02:28:00 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,61 +1,3 @@
-// import React from "react";
-// import { FaSearch } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import {useSelector, useState} from 'react-redux';
-
-
-// const Header = () => {
-//   const { currentUser } = useSelector((state) => state.user);
-//   return (
-//     <header className="bg-slate-200 shadow-md">
-//       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        
-//         <Link to='/'>
-//         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-//           <span className="text-slate-500">Urban</span>
-//           <span className="text-slate-700">Estate</span>
-//         </h1>
-//         </Link>
-
-//         <form className="bg-slate-100 rounded-lg p-3 flex items-center">
-//           <input
-//             type="text"
-//             placeholder="Search..."
-//             className="bg-trasparent focus:outline-none w-24 sm:w-64"
-//           />
-//           <FaSearch className="text-slate-600" />
-//         </form>
-
-//         <ul className="flex gap-4">
-
-//           <Link to="/">
-//           <li className="hidden sm:inline text-slate-700 hover:underline">
-//             Home
-//           </li>
-//           </Link>
-
-
-//           <Link to="/about">
-//           <li className="hidden sm:inline text-slate-700 hover:underline">
-//             About
-//           </li>
-//           </Link>
-
-//           <Link to='/sign-in'>
-//           {currentUser? (
-//             <img src={currentUser.avatar} alt='profile'/>
-//           ) : (
-//              <li className="text-slate-700 hover:underline">Sign in</li> 
-//           )}
-//           </Link>
-//         </ul>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -85,7 +27,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Urban</span>
+            <span className='text-slate-500'>Sahand</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
