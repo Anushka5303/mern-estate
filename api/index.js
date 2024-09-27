aa59d9d986a74df75dc466b4c79b8e4b32cde31f
Anushka Gupta <128627834+Anushka5303@users.noreply.github.com>
2024-09-27 10:50:34 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,46 +1,100 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
+// dotenv.config();
+
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js'
+// import listingRouter from  './routes/listing.route.js'
+
+// const app= express();
+
+// app.use(express.static(path.join(__dirname,'client/dist')));
+
+// app.get('*',(req,res) => {
+//     res.sendFile(path.join(__dirname,'client','dist','index.html'));
+// })
+
+
+// mongoose.connect(process.env.MONGO)
+// .then(()=>{
+//     console.log('Connected to MONGOdb');
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+
+// const __dirname = path.resolve();
+
+
+// app.use(express.json());
+
+// app.use(cookieParser());
+
+
+// app.listen(3000,()=>{
+//     console.log('Server is running on port 3000!!!');
+// }); 
+
+// app.use('/api/user',userRouter);
+// app.use('/api/auth',authRouter);
+// app.use('/api/listing',listingRouter);
+
+// app.use((err, req, res, next) => {
+//     const statusCode = err.statusCode || 500;
+//     const message = err.message || 'Internal Server Error';
+//     return res.status(statusCode).json({
+//         success: false,
+//         statusCode,
+//         message
+//     });
+// });
+
+
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
-import listingRouter from  './routes/listing.route.js'
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 
-const app= express();
+const app = express();
 
-app.use(express.static(path.join(__dirname,'client/dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
-})
-
-
-mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log('Connected to MONGOdb');
-})
-.catch((err)=>{
-    console.log(err);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-const __dirname = path.resolve();
-
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000!!!');
-}); 
+});
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
@@ -51,3 +105,4 @@ app.use((err, req, res, next) => {
         message
     });
 });
+
